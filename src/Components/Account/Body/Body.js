6c692ac676ab344b09e05a359cc24a4767e7d935
nek00oo <yakovenkovalera2017@yandex.ru>
2024-02-01 16:31:11 +0300

    fix

--- src/Components/Account/Body/Body.js
+++ src/Components/Account/Body/Body.js
@@ -4,10 +4,10 @@ import BorderButton from "../../UI/Button/BorderButton";
 import PlaceHolder from "../../UI/PlaceHolder/PlaceHolder";
 import {useState} from "react";
 const Body = () => {
-    const [name, setName] = useState("ad")
-    const [login, setLogin] = useState("ad")
-    const [height, setHeight] = useState("ad")
-    const [weight, setWeight] = useState("150кг")
+    const [name, setName] = useState("Константин")
+    const [login, setLogin] = useState("Kostya")
+    const [height, setHeight] = useState("1.8м")
+    const [weight, setWeight] = useState("80кг")
     return (
         <div className={s.Body}>
             <div className={s.Form}>
