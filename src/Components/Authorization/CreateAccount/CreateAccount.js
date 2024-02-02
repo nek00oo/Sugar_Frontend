d823ced949b6502714c719cc0de6af6492a5eac4
nek00oo <yakovenkovalera2017@yandex.ru>
2024-02-02 16:05:49 +0300

    Header : create NavLink in account, implemented create account (без линка пока)

---
+++ src/Components/Authorization/CreateAccount/CreateAccount.js
@@ -0,0 +1,14 @@
+import s from "./CreateAccount.module.css"
+import BorderButton from "../../UI/Button/BorderButton";
+const CreateAccount = () => {
+    return (
+        <div className={s.Body}>
+            <div className={s.Form}>
+                <div><h1 className={s.Header}>Ещё нет аккаунта ?</h1></div>
+                    <BorderButton>Cоздать</BorderButton>
+            </div>
+        </div>
+    );
+}
+
+export default CreateAccount;
---
+++ src/Components/Statistics/Body/Body.js
@@ -0,0 +1,13 @@
+import s from "./Body.module.css"
+
+const Body = () => {
+    return (
+        <div className={s.Body}>
+            <div className={s.Form}>
+                <p></p>
+            </div>
+        </div>
+    );
+}
+
+export default Body;
