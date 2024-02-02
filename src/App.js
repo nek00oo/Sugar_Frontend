bbce1e27f9e410d4bc470eb9af63fcd661994baf
Dmitrii Pligin <D-pligin@mail.ru>
2024-02-02 12:35:47 +0300

    add: Authcontext, UserContext, route separation

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import './App.css';
 
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Router from "./Router";
+import {AuthContext} from "./Context/AuthContext";
+import {useState} from "react";
+import {UserContext} from "./Context/UserContext";
 
 function App() {
+    const [Auth, setAuth] = useState(false)
+    const [userName, setUserName] = useState("Стас Барецкий")
+    const [userLogin, setUserLogin] = useState("stas@baretskii.su")
+    const [userHeight, setUserHeight] = useState("1.75м")
+    const [userWeight, setUserWeight] = useState("200кг")
+
   return (
-    <BrowserRouter>
-        <Router/>
-    </BrowserRouter>
+      <UserContext.Provider value={{userName, userLogin, userHeight, userWeight, setUserName, setUserLogin, setUserHeight, setUserWeight}}>
+            <AuthContext.Provider value={{Auth, setAuth}}>
+                <BrowserRouter>
+                    <Router/>
+                </BrowserRouter>
+            </AuthContext.Provider>
+      </UserContext.Provider>
   );
 }
 
--- src/Router.js
+++ src/Router.js
@@ -2,21 +2,23 @@ import {Route, Routes} from "react-router-dom";
 import Register from "./Components/Register/Register";
 import Authorization from "./Components/Authorization/Authorization";
 import Account from "./Components/Account/Account";
+import {useContext} from "react";
+import {AuthContext} from "./Context/AuthContext";
 
 const Router = () => {
-    const Auth = true;
+    const {Auth} = useContext(AuthContext)
 
     return (
         Auth
         ?
         <Routes>
-            <Route path="/register" element={<Register/>}></Route>
-            <Route path="/authorization" element={<Authorization/>}></Route>
-            <Route path="*" element={<Register/>}></Route>
+            <Route path="/account" element={<Account/>}></Route>
         </Routes>
         :
         <Routes>
-            <Route path="/account" element={<Account/>}></Route>
+            <Route path="/register" element={<Register/>}></Route>
+            <Route path="/authorization" element={<Authorization/>}></Route>
+            <Route path="*" element={<Register/>}></Route>
         </Routes>
     )
 }
